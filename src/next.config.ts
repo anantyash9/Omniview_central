12ae772f7aba9b6da1a5a1b98f9cff8c74b00e17
Anant Yash <anantyash9@gmail.com>
2025-07-26 09:48:07 +0000

    use this AIzaSyDiRAwA8UfHsCnwFu4LUQAVEfNi5R4LlLU

---
+++ src/next.config.ts
@@ -0,0 +1,30 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyDiRAwA8UfHsCnwFu4LUQAVEfNi5R4LlLU',
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
