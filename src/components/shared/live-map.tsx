4af6d591d84300919f884db6f24f778e88b41385
Anant Yash <anantyash9@gmail.com>
2025-07-27 03:08:22 +0000

    reduce the size of the heatmap

--- src/components/shared/live-map.tsx
+++ src/components/shared/live-map.tsx
@@ -76,10 +76,10 @@ const HeatmapLayer = ({
         const zoom = map.getZoom();
         if (zoom) {
             // Scale the radius based on zoom level for a more consistent feel
-            const newRadius = 80 * Math.pow(2, zoom - 17);
+            const newRadius = 40 * Math.pow(2, zoom - 17);
             heatmap.set('radius', newRadius);
         } else {
-            heatmap.set('radius', 80);
+            heatmap.set('radius', 40);
         }
     };
 
