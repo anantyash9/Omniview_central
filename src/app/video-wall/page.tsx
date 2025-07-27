84643a4e248efa0950a0473b46bef77e29adfe3e
Anant Yash <anantyash9@gmail.com>
2025-07-27 03:37:23 +0000

    make each video in the video wall bigger . 3 videos in a row instead of

--- src/app/video-wall/page.tsx
+++ src/app/video-wall/page.tsx
@@ -29,7 +29,7 @@ export function VideoWallContent() {
           </Button>
       </div>
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {cameras.map((camera) => (
           <Card key={camera.id}>
               <CardHeader className="p-3">
